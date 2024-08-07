39ffbb1bcdfeaf12789b0de5b466629f320f3538
SherwoodCode <177494395+SherwoodCode@users.noreply.github.com>
2024-08-07 10:16:36 -0500

    game widget settings

---
+++ lib/presentations/screens/my_game/my_game.dart
@@ -0,0 +1,22 @@
+import 'package:flame/game.dart';
+import 'package:flame_basic/presentations/screens/flame_sprite/simple_sprite_flame.dart';
+import 'package:flutter/cupertino.dart';
+
+class MyGame extends FlameGame {
+  @override
+  Future<void>? onLoad() async {
+    await add(SimpleSpriteFlame());
+    return super.onLoad();
+  }
+}
+
+
+class MyGameWidget extends StatelessWidget {
+  const MyGameWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GameWidget(game: MyGame());
+  }
+
+}
