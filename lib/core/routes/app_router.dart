edea79497122ef3109525d4864da878e42511cc5
SherwoodCode <177494395+SherwoodCode@users.noreply.github.com>
2024-08-08 09:18:37 -0500

    canvas reactangule complet

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flame_basic/presentations/screens/flame_basic/canvas_circle/canvas_circle.dart';
+import 'package:flame_basic/presentations/screens/flame_basic/canvas_rectangulo/canvas_rectangulo.dart';
 import 'package:flame_basic/presentations/screens/flame_basic/flame_sprite/simple_sprite_flame.dart';
 import 'package:flame_basic/presentations/screens/home/home_screen.dart';
 import 'package:go_router/go_router.dart';
@@ -15,5 +16,9 @@ final appRouter = GoRouter(routes: [
   GoRoute(
     path: '/circle',
     builder: (context, state) => const MyGameCircle(),
+  ),
+  GoRoute(
+    path: '/rectangule',
+    builder: (context, state) => const MyGameReactangulo(),
   ),
 ]);
